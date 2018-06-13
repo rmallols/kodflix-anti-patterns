5893df22f2385af8e7e7f83b7f67f3c619ad604c
Ricardo Mallols <ricardo.mallols@gmail.com>
2018-06-13 13:04:57 +0100

    Add 6 TV show titles #3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
-import theMistCover from './the-mist-cover.jpg';
+import blackMirror from './covers/black-mirror.jpg';
+import breakingBad from './covers/breaking-bad.jpg';
+import deathNote from './covers/death-note.jpg';
+import gameOfTHrones from './covers/game-of-thrones.jpg';
+import theWalkingDead from './covers/the-walking-dead.jpg';
+import theWire from './covers/the-wire.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <img src={theMistCover} className='image-cover' />
-      </div>
+        <div className='image-cover-row'>
+          <div className='image-cover-row-item'>
+            <h1>Black Mirror</h1>
+          </div>
+          <div className='image-cover-row-item'>
+            <h1>Breaking Bad</h1>
+          </div>
+          <div className='image-cover-row-item'>
+            <h1>Death Note</h1>
+          </div>
+        </div>
+        <div className='image-cover-row'>
+          <div className='image-cover-row-item'>
+            <h1>Game of Thrones</h1>
+          </div>
+          <div className='image-cover-row-item'>
+            <h1>The Walking Dead</h1>
+          </div>
+          <div className='image-cover-row-item'>
+            <h1>The Wire</h1>
+          </div>
+        </div>
+      </div >
     );
   }
 }
