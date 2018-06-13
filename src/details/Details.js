f720728f07eff699dd88add0dfc72f499d259cd0
Ricardo Mallols <ricardo.mallols@gmail.com>
2018-06-13 19:05:39 +0100

    Introduce routing / navigation #8

---
+++ src/details/Details.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Details.css';
+
+export default class Details extends React.Component {
+
+    render() {
+        return (
+            <h1>
+                Hello, this will be the details page for each Movie 
+                & TV show :)
+            </h1>
+        )
+    }
+}
--- src/gallery/cover/Cover.js
+++ src/gallery/cover/Cover.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Cover.css';
 
 export default class Cover extends React.Component {
 
     render() {
         return (
-            <div className='cover'>
+            <Link to='/details' className='cover'>
                 <img src={this.props.image} alt='' />
                 <div className='cover-overlay'>
                     <h1>{this.props.title}</h1>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
