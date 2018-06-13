d1ff1aa69e54357d0cfc66377496d813b96ca39c
Ricardo Mallols <ricardo.mallols@gmail.com>
2018-06-13 18:26:41 +0100

    Componentize the TV show element #7

--- src/App.js
+++ src/App.js
@@ -1,55 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import blackMirror from './covers/black-mirror.jpg';
-import breakingBad from './covers/breaking-bad.jpg';
-import deathNote from './covers/death-note.jpg';
-import gameOfTHrones from './covers/game-of-thrones.jpg';
-import theWalkingDead from './covers/the-walking-dead.jpg';
-import theWire from './covers/the-wire.jpg';
+import Cover from './cover/Cover';
+import blackMirror from './cover/images/black-mirror.jpg';
+import breakingBad from './cover/images/breaking-bad.jpg';
+import deathNote from './cover/images/death-note.jpg';
+import gameOfThrones from './cover/images/game-of-thrones.jpg';
+import theWalkingDead from './cover/images/the-walking-dead.jpg';
+import theWire from './cover/images/the-wire.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <div className='image-cover-row'>
-          <div className='image-cover-row-item'>
-            <img src={blackMirror}  alt='' />
-            <div className='image-cover-row-item-overlay'>
-              <h1>Black Mirror</h1>
-            </div>
-          </div>
-          <div className='image-cover-row-item'>
-            <img src={breakingBad}  alt='' />
-            <div className='image-cover-row-item-overlay'>
-              <h1>Breaking Bad</h1>
-            </div>
-          </div>
-          <div className='image-cover-row-item'>
-            <img src={deathNote} alt='' />
-            <div className='image-cover-row-item-overlay'>
-              <h1>Death Note</h1>
-            </div>
-          </div>
+          <Cover image={blackMirror} title='Black Mirror' />
+          <Cover image={breakingBad} title='Breaking Bad' />
+          <Cover image={deathNote} title='Death Note' />
         </div>
         <div className='image-cover-row'>
-          <div className='image-cover-row-item'>
-            <img src={gameOfTHrones}  alt='' />
-            <div className='image-cover-row-item-overlay'>
-              <h1>Game of Thrones</h1>
-            </div>
-          </div>
-          <div className='image-cover-row-item'>
-            <img src={theWalkingDead}  alt='' />
-            <div className='image-cover-row-item-overlay'>
-              <h1>The Walking Dead</h1>
-            </div>
-          </div>
-          <div className='image-cover-row-item'>
-            <img src={theWire} alt='' />
-            <div className='image-cover-row-item-overlay'>
-              <h1>The Wire</h1>
-            </div>
-          </div>
+          <Cover image={gameOfThrones} title='Game of Thrones' />
+          <Cover image={theWalkingDead} title='The Walking Dead' />
+          <Cover image={theWire} title='The Wire' />
         </div>
       </div>
     );
