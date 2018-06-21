93349028254e8ce1ce91b18024400cd38d2bb594
Ricardo Mallols <ricardo.mallols@gmail.com>
2018-06-21 16:58:23 +0100

    Add support for customer analytics! #31

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import './App.css';
 import Gallery from './gallery/Gallery';
 import Details from './details/Details';
 import NotFound from './not-found/NotFound';
 
 class App extends Component {
+
+  constructor({ history, location }) {
+    super();
+    ReactGA.initialize('UA-121194998-1');
+    this.trackPageView(location);
+    history.listen((location) => this.trackPageView(location));
+  }
+
+  trackPageView(location) {
+    ReactGA.pageview(location.pathname + location.search + location.hash);
+  }
+
   render() {
     return (
-      <Router>
-        <div className='App'>
-          <Switch>
-            <Route exact path='/' component={Gallery} />
-            <Route path='/not-found' component={NotFound} />
-            <Route path='/:showId' component={Details} />
-          </Switch>
-        </div>
-      </Router>
+      <div className='App'>
+        <Switch>
+          <Route exact path='/' component={Gallery} />
+          <Route path='/not-found' component={NotFound} />
+          <Route path='/:showId' component={Details} />
+        </Switch>
+      </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
